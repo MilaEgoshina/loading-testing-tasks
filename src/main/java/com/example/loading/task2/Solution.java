0e5ed9cb38dab153615dc8b545a4fe1f13b48ef2
mila <milavoronina1997@mail.ru>
2024-05-14 00:19:06 +0500

    dev: correct all mistakes to fullfil second  task

--- src/main/java/com/example/loading/task2/Solution.java
+++ src/main/java/com/example/loading/task2/Solution.java
@@ -1,5 +1,16 @@
 package com.example.loading.task2;
 
 public class Solution {
-// Эта строка не нужна, так как значение x переопределяется
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 7;
+
+        y = y + 2 * x;
+        x = x * 3;
+        // x = x * 16; - Эта строка не нужна, так как значение x переопределяется
+        // y = y + 2 * x; Эта строка не нужна, так как значение y переопределяется
+        y = x - y;
+
+        System.out.println(y);
+    }
 }
